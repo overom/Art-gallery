72fec84bbaa1b6d26566f7fdb68f02eed7baa4d4
Romain Denamur <romain.denamur@gmail.com>
2018-12-19 16:42:22 +0100

    test deploy

--- index.js
+++ index.js
@@ -90,6 +90,13 @@ app.use((error, req, res, next) => {
     error: error,
   });
 });
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5001;
 
